b642e2c40e4e2e17b843f183a306ffa1cc8cfe9b
Szedlák Tamás <tamas94@protonmail.com>
2023-05-01 00:06:28 +0200

    fix:delete button could delete everywhere in the text

--- funcs/deleteChar.js
+++ funcs/deleteChar.js
@@ -1,7 +1,21 @@
 function deleteChar() {
-  $('#inputField').val(
-    $('#inputField')
-      .val()
-      .slice(0, $('#inputField').val().length - 1)
-  );
+  let beginIndex = $('#inputField')[0].selectionStart;
+  let endIndex = $('#inputField')[0].selectionEnd;
+  if (beginIndex === endIndex && beginIndex !== 0) {
+    $('#inputField').val(
+      $('#inputField')
+        .val()
+        .slice(0, beginIndex - 1) +
+        $('#inputField').val().slice(beginIndex, $('#inputField').val().length)
+    );
+    $('#inputField')[0].selectionStart = beginIndex - 1;
+    $('#inputField')[0].selectionEnd = beginIndex - 1;
+  } else {
+    $('#inputField').val(
+      $('#inputField').val().slice(0, beginIndex) +
+        $('#inputField').val().slice(endIndex, $('#inputField').val().length)
+    );
+    $('#inputField')[0].selectionStart = beginIndex;
+    $('#inputField')[0].selectionEnd = beginIndex;
+  }
 }
